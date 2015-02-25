a24e5abcac9e12d48d642dee8b3c0f48fdf47aec
SpencerMcKimson <spencer.mckimson@gmail.com>
2015-02-25 12:31:45 -0800

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            if (args.Length == 0)
+                throw new ArgumentNullException();
+
+            string binary = args[0];
+            //check that the input is proper binary
+            if (binary.Length % 8 != 0)
+                throw new ArgumentException("Input was not valid binary", "binary");
+
+            for (int i = 0; i < binary.Length; i+=8)
+            {
+                string t = binary.Substring(i, 8);
+                output += (char)Convert.ToByte(t, 2);
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
